b8aa45864b94d09c299a5ad70eef17eaaa654eb5
wanghxing <1079563056@qq.com>
2020-08-28 17:28:46 +0800

    20200828

--- vue.config.js
+++ vue.config.js
@@ -2,5 +2,9 @@
 const ENV = process.env.NODE_ENV;
 
 module.exports = {
-  publicPath: ENV === "production" ? "" : "/vuetest-a/" // 关键代码
+  // publicPath: ENV === "production" ? "" : "/vuetest-a/", // 关键代码
+  publicPath:'./', // 基本路径
+  outputDir:"docs",  // 输出文件目录
+  assetsDir: './assets',
+  indexPath: 'index.html'
 };
